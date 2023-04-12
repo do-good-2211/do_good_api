77698c942226d6fcceb5a468838c4155f1133d7d
KaraJH <karas.jones@yahoo.com>
2023-04-11 20:54:26 -0600

    feature: Bored API Service methods

---
+++ app/services/bored_service.rb
@@ -0,0 +1,12 @@
+class BoredService
+  def fetch_api
+    response = conn.get 
+    JSON.parse(response.body, symbolize_names: true)
+
+  end
+
+  private
+    def conn 
+      Faraday.new(url: "http://www.boredapi.com/api/activity?type=charity")
+    end
+end
