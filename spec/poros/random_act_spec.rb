823ab5b5f2bdda774bd25a580cab807144f9e4cb
KaraJH <karas.jones@yahoo.com>
2023-04-11 22:16:46 -0600

    test: Random Act objects

---
+++ spec/poros/random_act_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe RandomAct do 
+  describe "instance methods" do 
+    context "#initialize" do 
+      it "exists and has attributes" do 
+        random_act = RandomAct.new(["1", "2", "3"])
+
+        expect(random_act.deed_names).to eq(["1", "2", "3"])
+        expect(random_act.id).to eq(nil)
+      end
+    end
+  end
+end
