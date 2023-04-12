a1d3247655abd146c930bc29a68c5118c5e2b0ba
KaraJH <karas.jones@yahoo.com>
2023-04-11 22:19:03 -0600

    feat: bored facade with 3 acts method

---
+++ app/facades/bored_facade.rb
@@ -0,0 +1,21 @@
+require "ostruct"
+
+class BoredFacade 
+  attr_reader :bored_service
+
+  def initialize
+    @bored_service = BoredService.new
+  end
+
+  def get_3_acts 
+    acts_collection = [] 
+    3.times do 
+      acts_collection << bored_service.fetch_api
+    end
+
+    three_acts = acts_collection.map do |act_info_hash|
+      act_info_hash[:activity]
+    end
+    RandomAct.new(three_acts)
+  end
+end
