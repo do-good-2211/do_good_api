08cfc1722299335599fd1291ee055b090e5c6a5a
Melony Franchini <meltravelz@gmail.com>
2023-04-12 16:51:56 -0600

    feat: Add error message handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :render_invalid_response
+
+  def render_invalid_response(exception)
+    render json: ErrorSerializer.new(exception.message).invalid_request, status: 404
+  end
 end
