698ab6680bb572ca30fdf1882bb0d6918a4b8126
KaraJH <karas.jones@yahoo.com>
2023-04-12 09:12:41 -0600

    fix: Bored facade spec assertions

--- spec/facades/bored_facade_spec.rb
+++ spec/facades/bored_facade_spec.rb
@@ -15,7 +15,10 @@ RSpec.describe BoredFacade do
       it "returns an array of 3 random act objects" do 
         bored_facade = BoredFacade.new 
 
-        expect(bored_facade.get_3_acts.first).to be_a(RandomAct)
+        expect(bored_facade.get_3_acts).to be_a(RandomAct)
+        expect(bored_facade.get_3_acts.deed_names).to be_an(Array)
+        expect(bored_facade.get_3_acts.deed_names.count).to eq(3)
+        expect(bored_facade.get_3_acts.deed_names.first).to be_a(String)
       end
     end
   end
