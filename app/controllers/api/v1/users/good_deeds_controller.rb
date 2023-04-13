4db4d285a37e391782c081fba2b47500ec8ba689
Melony Franchini <meltravelz@gmail.com>
2023-04-12 20:12:36 -0600

    feat: create good deeds controller #create & rescue_from error handling

--- app/controllers/api/v1/users/good_deeds_controller.rb
+++ app/controllers/api/v1/users/good_deeds_controller.rb
@@ -4,16 +4,11 @@
 class Api::V1::Users::GoodDeedsController < ApplicationController
   def create
     new_good_deed = GoodDeed.new(name: params[:deed_name], host_id: params[:user_id], date: params[:date], time: params[:time])
-
     if new_good_deed.save!
-      GoodDeed.last.add_participants(params[:attendees], params[:user_id].to_i)
-      
+      new_good_deed.add_participants(params[:attendees], params[:user_id].to_i)
       # user = User.find(params[:user_id]) & render json: UserSerializer.new(user) #, status: 201
       # render json: status: 201
-      
       render json: GoodDeedsSerializer.new(new_good_deed), status: 201
-    # else
-    #   render json: { errors: new_item.errors.full_messages.join(', ') }, status: :bad_request
     end
   end
 end
--- spec/requests/api/v1/users/good_deeds_request_spec.rb
+++ spec/requests/api/v1/users/good_deeds_request_spec.rb
@@ -14,54 +14,101 @@ RSpec.describe "Good Deeds Controller" do
         deed_name: deed_name, 
         date: date, 
         time: time, 
-        attendees: attendees
+        attendees: invitees
       }}
 
     describe "when sucessful" do 
       let(:deed_name) { "Wash neighbor's car." }
       let(:date) { "01-01-2023" }
       let(:time) { "17:00" }
-      let(:attendees) { [{ "user_id": invitee1.id }, { "user_id": invitee2.id }] }
 
-      it "creates a new Good Deed record when there are 1+ invitees" do # OR it only returns status 202?
-        headers = {"CONTENT_TYPE" => "application/json"}
-        post "/api/v1/users/#{user.id}/good_deeds", headers: headers, params: JSON.generate(good_deed_params)
+      describe "when 1+ invitees" do
+        let(:invitees) { [{ "user_id": invitee1.id }, { "user_id": invitee2.id }] }
+        
+        it "creates a new Good Deed record" do 
+          headers = {"CONTENT_TYPE" => "application/json"}
+          post "/api/v1/users/#{user.id}/good_deeds", headers: headers, params: JSON.generate(good_deed_params)
 
-        expect(response).to be_successful
-        parsed_data = JSON.parse(response.body, symbolize_names: true)
+          expect(response).to be_successful
+          parsed_data = JSON.parse(response.body, symbolize_names: true)
 
-        expect(parsed_data).to be_a(Hash)
-        expect(parsed_data[:data]).to be_a(Hash)
-        expect(parsed_data[:data].keys).to eq([:id, :type, :attributes])
-        expect(parsed_data[:data][:attributes]).to be_a(Hash)
-        expect(parsed_data[:data][:attributes].keys).to eq([:name, :media_link, :notes, :date, :time, :status, :host_id])
+          expect(parsed_data).to be_a(Hash)
+          expect(parsed_data[:data]).to be_a(Hash)
+          expect(parsed_data[:data].keys).to eq([:id, :type, :attributes])
+          expect(parsed_data[:data][:attributes]).to be_a(Hash)
+          expect(parsed_data[:data][:attributes].keys).to eq([:name, :media_link, :notes, :date, :time, :status, :host_id])
+        end
       end
 
-      it "creates a new Good Deed record when there are 0 invitees" do
-        headers = {"CONTENT_TYPE" => "application/json"}
-        post "/api/v1/users/#{@users[0].id}/good_deeds", headers: headers, params: JSON.generate(@good_deed_params)
+      describe "when 0 invitees" do
+        let(:invitees) { [] }
 
-        expect(response).to be_successful
-        parsed_data = JSON.parse(response.body, symbolize_names: true)
+        it "creates a new Good Deed record when there are 0 invitees" do
+          headers = {"CONTENT_TYPE" => "application/json"}
+          post "/api/v1/users/#{user.id}/good_deeds", headers: headers, params: JSON.generate(good_deed_params)
 
-        expect(parsed_data).to be_a(Hash)
-        expect(parsed_data[:data]).to be_a(Hash)
-        expect(parsed_data[:data].keys).to eq([:id, :type, :attributes])
-        expect(parsed_data[:data][:attributes]).to be_a(Hash)
-        expect(parsed_data[:data][:attributes].keys).to eq([:name, :media_link, :notes, :date, :time, :status, :host_id])
+          expect(response).to be_successful
+          parsed_data = JSON.parse(response.body, symbolize_names: true)
+
+          expect(parsed_data).to be_a(Hash)
+          expect(parsed_data[:data]).to be_a(Hash)
+          expect(parsed_data[:data].keys).to eq([:id, :type, :attributes])
+          expect(parsed_data[:data][:attributes]).to be_a(Hash)
+          expect(parsed_data[:data][:attributes].keys).to eq([:name, :media_link, :notes, :date, :time, :status, :host_id])
+        end
       end
     end
       
     describe "when NOT sucessful" do 
-      it "returns 404 when ??" do
-        headers = {"CONTENT_TYPE" => "application/json"}
-        post "/api/v1/users/#{@users[0].id}/good_deeds", headers: headers, params: JSON.generate(@good_deed_params)
+      describe "when attributes are missing" do
+        let(:deed_name) { "" }
+        let(:date) { "" }
+        let(:time) { "" }
+        let(:invitees) { [{ "user_id": invitee1.id }, { "user_id": invitee2.id }] }
 
-        parsed_data = JSON.parse(response.body, symbolize_names: true)
+        it "returns 404" do
+          headers = {"CONTENT_TYPE" => "application/json"}
+          post "/api/v1/users/#{user.id}/good_deeds", headers: headers, params: JSON.generate(good_deed_params)
 
-        expect(response).to have_http_status(404)
-        expect(parsed_data[:message]).to eq("Couldn't find Item with 'id'=0")
+          parsed_data = JSON.parse(response.body, symbolize_names: true)
 
+          expect(response).to have_http_status(404)
+          expect(parsed_data[:errors].first[:detail].first).to eq("Validation failed: Name can't be blank, Date can't be blank, Time can't be blank")
+        end
+      end
+
+      describe "when attributes are nil/invalid" do
+        let(:deed_name) { nil }
+        let(:date) { nil }
+        let(:time) { nil }
+        let(:invitees) { nil }
+
+        it "returns 404" do
+          headers = {"CONTENT_TYPE" => "application/json"}
+          post "/api/v1/users/#{user.id}/good_deeds", headers: headers, params: JSON.generate(good_deed_params)
+
+          parsed_data = JSON.parse(response.body, symbolize_names: true)
+
+          expect(response).to have_http_status(404)
+          expect(parsed_data[:errors].first[:detail].first).to eq("Validation failed: Name can't be blank, Date can't be blank, Time can't be blank")
+        end
+      end
+
+      describe "EDGE CASE: when invited user_ids are invalid" do
+        let(:deed_name) { "Wash neighbor's car." }
+        let(:date) { "01-01-2023" }
+        let(:time) { "17:00" }
+        let(:invitees) { [{ "user_id": invitee2.id }, { "user_id": 0 }] }
+
+        it "returns 404" do
+          headers = {"CONTENT_TYPE" => "application/json"}
+          post "/api/v1/users/#{user.id}/good_deeds", headers: headers, params: JSON.generate(good_deed_params)
+
+          parsed_data = JSON.parse(response.body, symbolize_names: true)
+
+          expect(response).to have_http_status(404)
+          expect(parsed_data[:errors].first[:detail].first).to eq("Couldn't find User with 'id'=0")
+        end
       end
     end
   end
