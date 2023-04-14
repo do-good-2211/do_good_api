69be3c22e758017994ebee10e5b288164a419df5
Huy Phan <huyphan520@gmail.com>
2023-04-14 12:57:47 -0500

    features: Sad Path test for destroy

--- spec/requests/api/v1/users/good_deeds_request_spec.rb
+++ spec/requests/api/v1/users/good_deeds_request_spec.rb
@@ -137,5 +137,24 @@ RSpec.describe "Good Deeds Controller" do
       expect(user2.good_deeds.count).to eq(1)
       expect(user1.good_deeds.count).to eq(0)
     end
+
+    it "couldn't delete if user or good deed does not exist" do
+      user = create(:user)
+      deed1 = create(:good_deed, host_id: user.id, status: 1)
+      user_deed = create(:user_good_deed, user_id: user.id, good_deed_id: deed1.id )
+ 
+      expect(user.good_deeds.count).to eq(1)
+
+      delete "/api/v1/users/#{user.id}/good_deeds/#{deed1.id}"
+
+      parse = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(404)
+      expect(parse).to be_a(Hash)
+      expect(parse.keys).to eq([:errors])
+      expect(parse[:errors]).to be_an(Array)
+      expect(parse[:errors][0].keys).to eq([:status, :title, :detail])
+      expect(parse[:errors][0][:detail]).to eq(["Completed good deed cannot be deleted"])
+    end
   end
 end
