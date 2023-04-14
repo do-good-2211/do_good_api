73b68c2474f5864f82668aea0b7145946c600194
Matt Enyeart <menyeart1@gmail.com>
2023-04-13 19:28:12 -0600

    refactor: change good deeds serializer attributes to match contract

--- app/serializers/good_deeds_serializer.rb
+++ app/serializers/good_deeds_serializer.rb
@@ -3,6 +3,6 @@
 # app/serializers/good_deeds_serializer.rb
 class GoodDeedsSerializer
   include JSONAPI::Serializer
-  attributes :name, :media_link, :notes, :date, :time, :status, :host_id
+  attributes :name, :media_link
 end
 
