0116dfdda170944d20a95e8523a446c82833c899
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-04-14 09:27:26 -0700

    test: create model test for .from_omniauth method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,7 +18,32 @@ RSpec.describe User, type: :model do
     it { should have_many(:good_deeds).through(:user_good_deeds) }
   end
 
-  describe "oauth" do 
-    
+  describe "class methods" do 
+    describe '.from_omniauth' do
+      it 'finds or creates a user' do
+        user_hash = {
+          provider: "google_oauth2",
+          uid: "100000000000000000000",
+          info: {
+            name: "John Smith",
+            email: "john@example.com",
+            first_name: "John",
+            last_name: "Smith",
+            image: "https://lh4.googleusercontent.com/photo.jpg",
+            urls: {
+              google: "https://plus.google.com/+JohnSmith"
+            }
+          }
+        }
+
+        expect(User.from_omniauth(user_hash)).to be_a User
+        user = User.from_omniauth(user_hash)
+
+        expect(user.uid).to eq('100000000000000000000')
+        expect(user.provider).to eq('google_oauth2')
+        expect(user.name).to eq('John Smith')
+        expect(user.email).to eq('john@example.com')
+      end
+    end
   end
 end
