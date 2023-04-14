318f99171a1292255ac9739f92512a86a87f2d47
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-04-14 10:49:08 -0700

    test: create validation tests for uid

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,8 +6,10 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :role }
     it { should validate_presence_of :email }
     it { should validate_presence_of :password }
+    it { should validate_presence_of :uid }
 
     it { should validate_uniqueness_of :email }
+    it { should validate_uniqueness_of :uid }
     it { should have_secure_password }
 
     it { should define_enum_for(:role).with_values([:user, :admin]) }
