46a7028d06cbac7a18ba7aeb133c001766f0eb29
Huy Phan <huyphan520@gmail.com>
2023-04-14 12:56:37 -0500

    features: Add helper method to check deed status

--- app/controllers/api/v1/users/good_deeds_controller.rb
+++ app/controllers/api/v1/users/good_deeds_controller.rb
@@ -2,6 +2,8 @@
 
 # app/controllers/api/v1/users/good_deeds_controller.rb
 class Api::V1::Users::GoodDeedsController < ApplicationController
+  before_action :check_deed_status, only: [:destroy]
+
   def create
     new_good_deed = GoodDeed.new(name: params[:deed_name], host_id: params[:user_id], date: params[:date], time: params[:time])
     return unless new_good_deed.save!
@@ -11,9 +13,17 @@ class Api::V1::Users::GoodDeedsController < ApplicationController
   end
 
   def destroy
-    user_deed = UserGoodDeed.find_by(good_deed_id: params[:id])
-    deed = GoodDeed.find(params[:id])
-    user_deed.destroy
-    deed.destroy
+    # binding.pry
+      user_deed = UserGoodDeed.find_by(good_deed_id: params[:id])
+      user_deed.destroy
+      @deed.destroy
+  end
+
+
+  def check_deed_status
+    @deed = GoodDeed.find(params[:id])
+    if @deed.status == "Completed"
+      render json: ErrorSerializer.new("Completed good deed cannot be deleted").invalid_request, status: 404
+    end
   end
 end
