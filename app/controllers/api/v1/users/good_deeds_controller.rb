c5cd4b73e0d5d929a0f1372e6cb5b60ec3d71389
Matt Enyeart <menyeart1@gmail.com>
2023-04-13 19:24:44 -0600

    refactor: change user good deed create to use good deed create serializer

--- app/controllers/api/v1/users/good_deeds_controller.rb
+++ app/controllers/api/v1/users/good_deeds_controller.rb
@@ -6,6 +6,6 @@ class Api::V1::Users::GoodDeedsController < ApplicationController
     new_good_deed = GoodDeed.new(name: params[:deed_name], host_id: params[:user_id], date: params[:date], time: params[:time])
     return unless new_good_deed.save!
     new_good_deed.add_participants(params[:attendees], params[:user_id].to_i)
-    render json: GoodDeedsSerializer.new(new_good_deed), status: 201
+    render json: GoodDeedsCreateSerializer.new(new_good_deed), status: 201
   end
 end
