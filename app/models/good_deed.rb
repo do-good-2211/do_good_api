97cac98e4a3fded80dd09b88c38a5fd8233d5767
Huy Phan <huyphan520@gmail.com>
2023-04-13 22:43:47 -0500

    refractor: Add dependent destroy to good deed

--- app/models/good_deed.rb
+++ app/models/good_deed.rb
@@ -2,7 +2,7 @@
 
 # app/models/good_deed.rb
 class GoodDeed < ApplicationRecord
-  has_many :user_good_deeds
+  has_many :user_good_deeds, dependent: :destroy
   has_many :users, through: :user_good_deeds
 
   validates_presence_of :name, :host_id, :date, :time, :status
