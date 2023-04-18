3d2cf534ab555f933b318b97fcddca96c6452135
KaraJH <karas.jones@yahoo.com>
2023-04-17 21:16:43 -0600

    feat: Add host name and attendees to user serializer attributes

--- app/serializers/good_deed_serializer.rb
+++ app/serializers/good_deed_serializer.rb
@@ -2,6 +2,10 @@ class GoodDeedSerializer
   include JSONAPI::Serializer
   attributes :name, :date, :time, :status, :notes, :media_link
 
+  attributes :host_name do |object|
+    object.host_name
+  end
+
   attributes :attendees do |object| 
     object.attendees
   end
--- app/serializers/users_serializer.rb
+++ app/serializers/users_serializer.rb
@@ -3,5 +3,10 @@
 # app/serializers/users_acts_serializer.rb
 class UsersSerializer
   include JSONAPI::Serializer
-  attributes :name, :role, :good_deeds, :email
+  attributes :name, :role, :email #:good_deeds, :email
+
+  attributes :good_deeds do |object|
+    # require 'pry'; binding.pry
+    GoodDeedSerializer.new(object.good_deeds)
+  end
 end
