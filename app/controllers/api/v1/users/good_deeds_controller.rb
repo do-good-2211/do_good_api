d9dd621106cf68ad78c848e689f822ba9528bc8a
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-04-20 09:41:39 -0700

    fix: update email sending for first time that status changes from in progress to complete

--- app/controllers/api/v1/users/good_deeds_controller.rb
+++ app/controllers/api/v1/users/good_deeds_controller.rb
@@ -31,16 +31,17 @@ class Api::V1::Users::GoodDeedsController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:user_id])
-    @good_deed = @user.good_deeds.find(params[:id])
-    @good_deed.update!(good_deed_params)
-    render json: GoodDeedSerializer.new(@good_deed)
+    @good_deed = GoodDeed.find(params[:id])
 
-    if @good_deed.status == "Completed"
+    @good_deed.assign_attributes(good_deed_params)
+    if @good_deed.status_changed?
       @good_deed.users.each do |user|
         UserNotifierMailer.send_completed_deed_email(user).deliver_now
       end
     end
+
+    @good_deed.update!(good_deed_params)
+    render json: GoodDeedSerializer.new(@good_deed)
   end
 
   private
